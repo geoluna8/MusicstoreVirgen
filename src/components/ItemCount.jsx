d75d0a4d13ee1d7cc20bb2b8e89b9b5c3236e499
System Administrator <root@MacBook-Pro-de-MacPro.local>
2022-01-13 22:43:13 -0700

    mejoras en ItemCount

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -12,23 +12,37 @@ const ItemCount = ({stock, initial}) => {
 
     const [itemsQty, setItemsQty] = useState(initial);
     const [totalItems, addToCart] = useState(0);
-        useEffect(() => {
+    useEffect(() => {
         console.log("Cambie el item de cantidad","ahora tengo", itemsQty);
         return () => {
           console.log("se desmonto el componente")
         }
       },[itemsQty, totalItems]);
 
-    return <Container>
+      const handleLessQty = () => {
+        if(itemsQty > 0){ setItemsQty(itemsQty - 1) }
+      }
+
+      const handlePlusQty = () => {
+        if(itemsQty < stock){ setItemsQty(itemsQty + 1) }
+      }
+
+      const handleAddToCart = () => {
+        if(stock > 0){ addToCart(totalItems + itemsQty) }
+      }
+
+    return (
+    <Container>
         <Row style={ rowStyle } xs={3}>
-            <Col style={ {paddingLeft: 0} }><Button style={ {float: "left"} } variant="primary" onClick={ () => { if(itemsQty > 0){ setItemsQty(itemsQty - 1)}} }>-</Button></Col>
+            <Col style={ {paddingLeft: 0} }><Button style={ {float: "left"} } variant="primary" onClick={ handleLessQty }>-</Button></Col>
             <Col style={ {marginTop: "6px"} }><span>{itemsQty}</span></Col>
-            <Col style={ {paddingRight: 0} }><Button style={ {float: "right"} } variant="primary" onClick={ () => { if(itemsQty < stock){setItemsQty(itemsQty + 1)}} }>+</Button></Col>
+            <Col style={ {paddingRight: 0} }><Button style={ {float: "right"} } variant="primary" onClick={ handlePlusQty }>+</Button></Col>
         </Row>
         <Row style={ {paddingTop: "15px", background: "#d3d3d326"} }>
-            <Button variant="success" onClick={ () => { if(stock > 0){addToCart(totalItems + itemsQty)} } }><FontAwesomeIcon icon={faCartPlus} /></Button>
+            <Button variant="success" onClick={ handleAddToCart }><FontAwesomeIcon icon={faCartPlus} /></Button>
         </Row>
     </Container> 
+    )
 }
 
 export default ItemCount;
